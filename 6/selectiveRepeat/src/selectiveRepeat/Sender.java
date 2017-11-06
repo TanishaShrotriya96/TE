7dd34cf210f6cf6f998df540ae79fdbcb73ed7ad
TanishaShrotriya <tanishashrotriya91@gmail.com>
2017-11-06 10:14:26 +0530

    selective repeat update

--- 6/selectiveRepeat/src/selectiveRepeat/Sender.java
+++ 6/selectiveRepeat/src/selectiveRepeat/Sender.java
@@ -22,6 +22,8 @@ public class Sender {
 		    dout=new DataOutputStream(S.getOutputStream());
 			
 			ArrayList<Integer> data = new ArrayList<Integer>();
+			ArrayList<Integer> total = new ArrayList<Integer>();
+			
 			
 			Scanner sc=new Scanner(System.in);
             int ack=-2,frame=0,window=0,fail=0;
@@ -55,17 +57,19 @@ public class Sender {
 				
 				if(resend==1) {
 					dout.writeInt(data.get(0));
+					total.add(data.get(0));
 				    resend =0;	
 				}
 				else {
 					dout.writeInt(data.get(count));
+					total.add(data.get(count));
 					count++;
 				}
 				try{
 					ack=din.readInt();
 				
 					if(ack==-1) {
-						System.out.println("Acknowledgement failed and resending packet "+data.toString());
+						System.out.println("Acknowledgement failed and resending packet "+data.get(0));
 						System.out.println("Writing : " + data.get(0));
 						resend=1;
 					}
@@ -86,6 +90,14 @@ public class Sender {
 				
 				
 			}
+			System.out.println(total.toString());
+	        int TotalPacketsCnt=0;
+			for(Integer e : total) {
+				if(e!=-2) {
+					TotalPacketsCnt=TotalPacketsCnt+1;
+				}
+			}
+			System.out.println("Total packets sent after packet failures : "+TotalPacketsCnt);
 			
 			S.close();
 			
@@ -96,12 +108,12 @@ public class Sender {
 	}
 	
 /*
- * Enter the frame size
+Enter the frame size
 10
 Enter the window size
-3
+4
 Enter the failure size
-5
+6
 Enter the next packet
 1
 Waiting for acknowledgement
@@ -110,34 +122,37 @@ Enter the next packet
 Waiting for acknowledgement
 Enter the next packet
 3
-Acknowledgement received for 1
+Waiting for acknowledgement
 Enter the next packet
 4
-Acknowledgement received for 2
+Acknowledgement received for 1
 Enter the next packet
 5
-Acknowledgement received for 3
+Acknowledgement received for 2
 Enter the next packet
 6
-Acknowledgement received for 4
+Acknowledgement received for 3
 Enter the next packet
 7
-Acknowledgement failed and resending packet [5, 6, 7]
-Writing : 5
+Acknowledgement received for 4
 Enter the next packet
 8
 Acknowledgement received for 5
 Enter the next packet
 9
-Acknowledgement received for 6
+Acknowledgement failed and resending packet 6
+Writing : 6
 Enter the next packet
 10
+Acknowledgement received for 6
 Acknowledgement received for 7
 Acknowledgement received for 8
-Acknowledgement failed and resending packet [9, 10, -2, -2]
-Writing : 9
 Acknowledgement received for 9
 Acknowledgement received for 10
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 6, 10, -2, -2, -2]
+Total packets sent after packet failures : 11
+
+//-2 represents a time delay caused by one packet
 
 
 RECEIVER SIDE :
@@ -145,17 +160,17 @@ RECEIVER SIDE :
  []
  [Packet:1 |failStatus:1 |ack:-1]
  [Packet:1 |failStatus:1 |ack:-1, Packet:2 |failStatus:2 |ack:-1]
- [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:-1, Packet:3 |failStatus:3 |ack:-1]
- [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:-1, Packet:4 |failStatus:4 |ack:-1]
- [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:-1, Packet:5 |failStatus:5 |ack:-1]
- [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:4, Packet:5 |failStatus:5 |ack:-1, Packet:6 |failStatus:1 |ack:-1]
- [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:4, Packet:-1 |failStatus:-1 |ack:-1, Packet:6 |failStatus:1 |ack:-1, Packet:7 |failStatus:2 |ack:-1]
- [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:4, Packet:5 |failStatus:3 |ack:5, Packet:6 |failStatus:1 |ack:-1, Packet:7 |failStatus:2 |ack:-1]
- [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:4, Packet:5 |failStatus:3 |ack:5, Packet:6 |failStatus:1 |ack:6, Packet:7 |failStatus:2 |ack:-1, Packet:8 |failStatus:4 |ack:-1]
- [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:4, Packet:5 |failStatus:3 |ack:5, Packet:6 |failStatus:1 |ack:6, Packet:7 |failStatus:2 |ack:7, Packet:8 |failStatus:4 |ack:-1, Packet:9 |failStatus:5 |ack:-1]
- [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:4, Packet:5 |failStatus:3 |ack:5, Packet:6 |failStatus:1 |ack:6, Packet:7 |failStatus:2 |ack:7, Packet:8 |failStatus:4 |ack:8, Packet:9 |failStatus:5 |ack:-1, Packet:10 |failStatus:1 |ack:-1]
- [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:4, Packet:5 |failStatus:3 |ack:5, Packet:6 |failStatus:1 |ack:6, Packet:7 |failStatus:2 |ack:7, Packet:8 |failStatus:4 |ack:8, Packet:-1 |failStatus:-1 |ack:-1, Packet:10 |failStatus:1 |ack:-1, Packet:-2 |failStatus:2 |ack:-1]
- [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:4, Packet:5 |failStatus:3 |ack:5, Packet:6 |failStatus:1 |ack:6, Packet:7 |failStatus:2 |ack:7, Packet:8 |failStatus:4 |ack:8, Packet:9 |failStatus:3 |ack:9, Packet:10 |failStatus:1 |ack:-1, Packet:-2 |failStatus:2 |ack:-1]
+ [Packet:1 |failStatus:1 |ack:-1, Packet:2 |failStatus:2 |ack:-1, Packet:3 |failStatus:3 |ack:-1]
+ [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:-1, Packet:3 |failStatus:3 |ack:-1, Packet:4 |failStatus:4 |ack:-1]
+ [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:-1, Packet:4 |failStatus:4 |ack:-1, Packet:5 |failStatus:5 |ack:-1]
+ [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:-1, Packet:5 |failStatus:5 |ack:-1, Packet:6 |failStatus:6 |ack:-1]
+ [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:4, Packet:5 |failStatus:5 |ack:-1, Packet:6 |failStatus:6 |ack:-1, Packet:7 |failStatus:1 |ack:-1]
+ [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:4, Packet:5 |failStatus:5 |ack:5, Packet:6 |failStatus:6 |ack:-1, Packet:7 |failStatus:1 |ack:-1, Packet:8 |failStatus:2 |ack:-1]
+ [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:4, Packet:5 |failStatus:5 |ack:5, Packet:-1 |failStatus:-1 |ack:-1, Packet:7 |failStatus:1 |ack:-1, Packet:8 |failStatus:2 |ack:-1, Packet:9 |failStatus:3 |ack:-1]
+ [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:4, Packet:5 |failStatus:5 |ack:5, Packet:6 |failStatus:4 |ack:6, Packet:7 |failStatus:1 |ack:-1, Packet:8 |failStatus:2 |ack:-1, Packet:9 |failStatus:3 |ack:-1]
+ [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:4, Packet:5 |failStatus:5 |ack:5, Packet:6 |failStatus:4 |ack:6, Packet:7 |failStatus:1 |ack:7, Packet:8 |failStatus:2 |ack:-1, Packet:9 |failStatus:3 |ack:-1, Packet:10 |failStatus:5 |ack:-1]
+ [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:4, Packet:5 |failStatus:5 |ack:5, Packet:6 |failStatus:4 |ack:6, Packet:7 |failStatus:1 |ack:7, Packet:8 |failStatus:2 |ack:8, Packet:9 |failStatus:3 |ack:-1, Packet:10 |failStatus:5 |ack:-1, Packet:-2 |failStatus:6 |ack:-1]
+ [Packet:1 |failStatus:1 |ack:1, Packet:2 |failStatus:2 |ack:2, Packet:3 |failStatus:3 |ack:3, Packet:4 |failStatus:4 |ack:4, Packet:5 |failStatus:5 |ack:5, Packet:6 |failStatus:4 |ack:6, Packet:7 |failStatus:1 |ack:7, Packet:8 |failStatus:2 |ack:8, Packet:9 |failStatus:3 |ack:9, Packet:10 |failStatus:5 |ack:-1, Packet:-2 |failStatus:6 |ack:-1, Packet:-2 |failStatus:1 |ack:-1]
 
  */
 }
