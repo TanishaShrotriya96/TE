515dc3a3af62a3e03824eeb8ba50dbb20eb00db5
tanishashrotriya91@gmail.com <tanishashrotriya91@gmail.com>
2018-02-06 23:35:54 +0530

    Assign3 Macro Pass1

---
+++ Assign3/src/Param.java
@@ -0,0 +1,17 @@
+
+public class Param {
+
+	int num;
+	String name;
+	String code;
+	
+	public Param(){
+		num=0;
+		name=null;
+		code=null;
+	}
+	
+	public String toString(){
+		return name;
+	}
+}
